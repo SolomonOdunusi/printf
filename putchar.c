279054fe64c4762299c8f246609d0bcbecdf9b5f
SolomonOdunusi <solomonodunusi@gmail.com>
2023-04-24 03:21:37 -0700

    new commit

---
+++ putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - print character
+ * @c: char
+ * Return: return On success 1
+*/
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
