d7e1d49554ec2879eee718ca26d19c3c01d71e21
Pheenah <my>
2023-04-24 05:51:03 -0700

    fmt_size.c

---
+++ print_percent.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_percent - Prints a percent sign
+* @types: List of arguments
+* @buffer: Buffer array to handle print
+* @flags: Calculates active flags
+* @width: get width.
+* @precision: Precision specification
+* @size: Size specifier
+* Return: returns the number of chars printed
+*/
+int print_percent(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	return (write(1, "%%", 1));
+}
