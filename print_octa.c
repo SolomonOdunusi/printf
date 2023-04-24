679ba67cb10f11aedce62af6ed5b5488cfa343a3
Pheenah <my>
2023-04-24 12:46:16 -0700

    print_octa.c

--- print_octa.c
+++ print_octa.c
@@ -2,7 +2,7 @@
 
 /**
 * print_octal - Prints an unsigned number in octal notation
-* @types: Lista of arguments
+* @types: List of arguments
 * @buffer: Buffer array to handle print
 * @flags: Calculates active flags
 * @width: get width
